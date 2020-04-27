575e8395a58c3b53d3ccc998076f34cd42f8c3c7
ymynyg <ymynyg@163.com>
2020-04-27 19:09:46 +0800

    去哪儿

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // 设置代理
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8081',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
